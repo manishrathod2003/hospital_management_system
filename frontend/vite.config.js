4348d95105e3f882e818d67e70714bbe4f730bbe
Manish Rathod <manishrathod2003@gmail.com>
2025-09-06 18:30:26 +0530

    Updated backend code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://hospital-management-system-sqcs.onrender.com'
+      '/api': 'http://localhost:5000'
     }
   }
 })
