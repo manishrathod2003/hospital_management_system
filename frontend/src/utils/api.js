316bbaf16f3865e82cd41eadbb6144782d5f4644
Manish Rathod <manishrathod2003@gmail.com>
2025-09-06 18:10:45 +0530

    Updated backend code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: import.meta.env.VITE_API_URL,  // .env se value lega
 });
 
 export default api;
